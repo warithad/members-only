92ea8d55a880f8eef8c7ecaae512a2c8b7465dd9
warith <warith.ad02@gmail.com>
2023-03-05 23:52:01 +0100

    Add export statement to bottom of router files

--- server/src/routes/messageRoute.js
+++ server/src/routes/messageRoute.js
@@ -5,3 +5,5 @@ const message_controller = require('../controllers/messageController')
 router.get('/', message_controller.messages_list_get)
 router.post('/create', message_controller.message_create_post)
 router.delete('/delete/:id', message_controller.message_delete)
+
+module.exports = router;
--- server/src/routes/userRoute.js
+++ server/src/routes/userRoute.js
@@ -5,3 +5,5 @@ const user_controller = require('../controllers/userController')
 
 router.get('/', user_controller.user_get);
 router.put('/update/:id', user_controller.user_update_put);
+
+module.exports = router;
